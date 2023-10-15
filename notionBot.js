04e594b80428a4d45252e136f8b2d7d75fe5c019
Anton Lipatov <tonybass1414@yahoo.com>
2023-10-16 00:00:24 +0600

    4

--- notionBot.js
+++ notionBot.js
@@ -14,7 +14,7 @@ Handlers.commands(BotBase.bot);
 
 BotBase.bot.launch({ 
     webhook: { 
-        domain: `https://${BotBase.config.credentials.WEBHOOK_HOST}`, 
+        domain: `https://${BotBase.config.credentials.WEBHOOK_HOST}/${BotBase.config.credentials.TG_TOKEN}`, 
         port: Number(BotBase.config.credentials.WEBAPP_PORT) 
     } 
 });
