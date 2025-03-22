0f31376202f35b8836885ba5102187e140574ff1
lines14 <tonybass1414@yahoo.com>
2025-03-22 18:06:44 +0500

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+export default {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    mocha: true,
+  },
+  extends: "airbnb-base",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+};
